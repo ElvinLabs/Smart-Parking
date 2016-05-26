4e1c3958560a8825e1c32d0f1e21e72f7846ee5e
AjanthaB <chanaapkb@gmail.com>
2016-05-26 14:14:51 +0530

    Merge branch 'ajantha'
    # Conflicts:
    #	Server/routes/index.js
    #	Server/views/index.ejs

--- Server/models/place/placeModel.js
+++ Server/models/place/placeModel.js
@@ -4,8 +4,12 @@ var mongoose = require('mongoose'),
 var placeSchema = new Schema({
 	name:{type:String},
 	lat:{type:Number},
-    lng:{type:Number},
-    created:{type:Date}, 
+	lng:{type:Number},
+	created:{type:Date},
+	type:{type:String, enum:['indoor','outdoor']},
+	numOfSlots:{type:Number,min:0},
+	freeSlots:{type:Number,min:0},
+	isDisable:{type:Boolean, default:true}
 });
 
 module.exports = mongoose.model('Place', placeSchema);
